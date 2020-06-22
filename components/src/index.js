2b5f1c4ba6e73299b7c828e80c154d6c85e460a3
Robin Lee <robinleeaus@gmail.com>
2020-06-22 22:40:00 +1000

    36. Passing Props - Solutions

--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-    </div>  
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        text="Nice blog post!"
+        imgSrc={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        text="Good blog post!"
+        imgSrc={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        text="Excellent blog post!"
+        imgSrc={faker.image.avatar()}
+      />
+    </div>
   );
 };
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
